90fdc3b2d604d066f54f7e7748897a84c0aa83fc
hxreborn <hxreborn@duck.com>
2026-03-19 00:26:30 +0100

    refactor: use module name as log tag
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- app/src/main/kotlin/eu/hxreborn/tfs/ModuleConstants.kt
+++ app/src/main/kotlin/eu/hxreborn/tfs/ModuleConstants.kt
@@ -2,5 +2,5 @@ package eu.hxreborn.tfs
 
 object ModuleConstants {
     val prefsGroup: String = "${BuildConfig.APPLICATION_ID.substringAfterLast('.')}_prefs"
-    const val LOG_TAG: String = BuildConfig.APPLICATION_ID
+    const val LOG_TAG: String = "ThreeFingerSwipe"
 }
