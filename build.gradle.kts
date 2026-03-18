65e40b3d2bd61b48ae6b296bd387c197aa801fd8
hxreborn <hxreborn@duck.com>
2026-03-19 00:16:16 +0100

    build(gradle): remove libxposed submodule build tasks
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,20 +5,6 @@ plugins {
     alias(libs.plugins.aboutlibraries) apply false
 }
 
-tasks.register<Exec>("buildLibxposedApi") {
-    workingDir = file("libxposed/api")
-    commandLine("./gradlew", "publishToMavenLocal", "--no-daemon")
-}
-
-tasks.register<Exec>("buildLibxposedService") {
-    workingDir = file("libxposed/service")
-    commandLine("./gradlew", "publishToMavenLocal", "--no-daemon")
-}
-
-tasks.register("buildLibxposed") {
-    dependsOn("buildLibxposedApi", "buildLibxposedService")
-}
-
 tasks.register("assembleDebugRelease") {
     dependsOn(":app:assembleDebug", ":app:assembleRelease")
 }
