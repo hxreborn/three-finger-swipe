eb2466b90e8fc1ddbde774c3cf7f2427dac74899
hxreborn <hxreborn@duck.com>
2026-03-18 09:22:08 +0100

    build(gradle): simplify buildLibxposed tasks for API 101
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,38 +3,16 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.aboutlibraries) apply false
-    alias(libs.plugins.ktlint) apply false
 }
 
 tasks.register<Exec>("buildLibxposedApi") {
     workingDir = file("libxposed/api")
-    commandLine(
-        "./gradlew",
-        ":api:publishApiPublicationToMavenLocal",
-        "-x",
-        ":checks:compileKotlin",
-        "--no-daemon",
-    )
+    commandLine("./gradlew", "publishToMavenLocal", "--no-daemon")
 }
 
 tasks.register<Exec>("buildLibxposedService") {
     workingDir = file("libxposed/service")
-    // TODO: remove once https://github.com/libxposed/api/pull/51 is merged
-    val interfaceBuild = file("libxposed/service/interface/build.gradle.kts")
-    doFirst {
-        interfaceBuild.writeText(
-            interfaceBuild.readText().replace(
-                """namespace = "io.github.libxposed.service"""",
-                """namespace = "io.github.libxposed.iface"""",
-            ),
-        )
-    }
-    commandLine(
-        "./gradlew",
-        ":interface:publishInterfacePublicationToMavenLocal",
-        ":service:publishServicePublicationToMavenLocal",
-        "--no-daemon",
-    )
+    commandLine("./gradlew", "publishToMavenLocal", "--no-daemon")
 }
 
 tasks.register("buildLibxposed") {
